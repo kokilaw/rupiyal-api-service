4cd075bd275f18d10985e669a4fa99c9fb6a6460
kokilaw <kokilapc@gmail.com>
2024-04-28 03:35:06 +0530

    nit(): added end point to get historical currency data by currency code

--- src/main/java/io/kokilaw/rupiyal/controller/internal/ExchangeRateController.java
+++ src/main/java/io/kokilaw/rupiyal/controller/internal/ExchangeRateController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
